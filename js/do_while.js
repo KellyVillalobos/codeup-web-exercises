adabbbb953bc375faf33f7c35ab34a81baac2e66
Kelly Villalobos <kvdesign1978@gmail.com>
2018-03-22 17:02:33 -0500

    pushing loops exercises

---
+++ js/do_while.js
@@ -0,0 +1,25 @@
+var allCones = Math.floor(Math.random() * 50) + 50;
+
+var conesBought = Math.floor(Math.random() * 5) + 1;
+
+console.log("i have " + allCones + " this morning.");
+
+do {
+
+    if (allCones < conesBought) {
+        console.log("sorry I can't sell you " + conesBought + " I only have " + allCones);
+    } else {
+        allCones = allCones - conesBought;
+        console.log("i have " + allCones + " left.");
+    }
+} while (allCones > 0);
+
+console.log("Yay I sold all my cones!!");
+
+
+
+
+
+// This is how you get a random number between 50 and 100
+
+// This expression will generate a random number between 1 and 5
---
+++ js/while.js
@@ -0,0 +1,13 @@
+"use strict";
+
+
+//1. define the variable in this case would start with 1 to multiply by 2 in the body of the loop.
+//2. i is multiplied by 2 to get first number and then incremented times 2 to get i.
+//3. used the number after the last number to stop at the last number.
+var i = 1;
+while (i <= 32768) {
+    console.log('while loop iteration #' + i);
+    console.log(2 * i);
+    i = i * 2;
+}
+
