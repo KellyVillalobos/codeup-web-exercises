3f1345083529a5e213fb496221ff1f46d7c79ed6
Kelly Villalobos <kvdesign1978@gmail.com>
2018-03-23 11:15:01 -0500

    resolving issue

--- js/while.js
+++ js/while.js
@@ -5,7 +5,7 @@
 //2. i is multiplied by 2 to get first number and then incremented times 2 to get i.
 //3. used the number after the last number to stop at the last number.
 var i = 1;
-while (i <= 32768) {
+while (i <= 32768){
     console.log(2 * i);
     i = i * 2;
 }
