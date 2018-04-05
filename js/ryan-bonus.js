a943c422f0a0b248053e5361195cdc568ad21ef9
Kelly Villalobos <kvdesign1978@gmail.com>
2018-04-05 14:35:37 -0500

    init commit jquery exercise 1

--- js/ryan-bonus.js
+++ js/ryan-bonus.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
 
     // sum up all the expenses
 // get an average of all the expenses
@@ -8,23 +8,29 @@
         {itemName: "raspberry pi", cost: "$60"},
         {itemName: "Dell XPS laptop", cost: "$1,200"}
     ];
-        var totalCost = 0;
+    var totalCost = 0;
 
-        expenses.forEach(function (expense) {
-            var itemCost = (parseFloat(expense.cost.replace(/\$|,/g, '')));
-            totalCost += itemCost;
+    expenses.forEach(function (expense) {
+        var itemCost = (parseFloat(expense.cost.replace(/\$|,/g, '')));
+        totalCost += itemCost;
 
-        });
+    });
 
     console.log("Total expenses are: " + "$" + totalCost.toLocaleString());
-
-function getAverage(number){
-   var average = number / expenses.length;
-   return average;
- }
+    console.log("The average of the toal cost is:" + " $" + getAverage(totalCost).toLocaleString());
 
 
- console.log("The average of the toal cost is:" + " $" + getAverage(totalCost).toLocaleString());
+    function getAverage(number) {
+        var average = number / expenses.length;
+        return average;
+    }
+    var numbers = ([], [4,5], []);
+
+    for(var i = 0; i<numbers.length; i++){
+
+    }
+
+
 })();
 
 
