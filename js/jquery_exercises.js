aea427c42d002d2a7d8d3bce41d05f81a7635543
Kelly Villalobos <kvdesign1978@gmail.com>
2018-04-06 10:10:55 -0500

    jquery exercise 2 events

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,4 +1,12 @@
 'use strict';
-$("h1, li, p").css({
-    "background-color": "red"
-});
+
+function changeColor() {
+    if ($('h1').css('background-color') === "rgba(0, 0, 0, 0)") {
+        $('h1').css('background-color', 'red');
+    }else{
+        $('h1').css('background-color', 'transparent');
+    }
+console.log($('h1').css('background-color'));
+}
+
+$('h1').on('click', changeColor);
