39c56fff6a594507d9ed06c64e38beed69fc9eab
Kelly Villalobos <kvdesign1978@gmail.com>
2018-04-27 08:30:19 -0500

    pushing

--- js/promises.js
+++ js/promises.js
@@ -1,6 +1,6 @@
 'use strict';
 
-
+//const wait = aNumber => {}
 function wait(aNumber) {
     return new Promise((resolve) => {
         setTimeout(() => {
@@ -14,8 +14,12 @@ wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 
 
 
-
+//const request = fetch
 fetch('https://api.github.com/users/KellyVillalobos/events/public'    , {headers: {'Authorization': 'My Token'}})
+
+    //request
     .then(response => response.json())
-    .then((users) => console.log(users[0].created_at));
+    //.then(events => console.log(events))
+    //events[0].created_at
+    .then(users => console.log(users[0].created_at));
 
